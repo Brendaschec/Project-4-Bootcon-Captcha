b586c0b0beeeba1aed466b5091cef0e0c76eb388
aakyfun <aakhil.kassim@gmail.com>
2023-05-03 22:20:11 -0400

    Rename capaud to capsnd

---
+++ apps/captcha/capsnd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#### Captcha Sound Generator
+
+#### Globals
+# Somewhat Meaningless Version Info
+appVer = "0.1"
+appTitle = "Captcha Sound Generator"
+
+def main():
+  print(f"{appTitle} Version {appVer}")
+
+def CaptchaSound():
+  print("Not Implemented")
